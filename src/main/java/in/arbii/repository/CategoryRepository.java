af0b263fe9ca6cd48fa8bb57f4f8f9fbb57fb4a1
Arbaz4Sayyad <arbaz4sayyad@gmail.com>
2025-01-16 19:29:04 +0530

    save get category implemented

---
+++ src/main/java/in/arbii/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package in.arbii.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.arbii.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+
+}
